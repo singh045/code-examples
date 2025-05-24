b212628d7ee0e2fc3961654ed5119d2ad8260197
Madhu <madhusingh14061996@gmail.com>
2025-05-24 19:12:36 +0530

    age licence validations

---
+++ age-license.py
@@ -0,0 +1,16 @@
+# Age and License Eligibility
+# Ask for a person's age.
+# If they are 18 or older, ask if they have a driving license (yes/no)
+# Print "Can drive" only if both conditions are met
+
+
+num=int(input("Enter a number"))
+a="Yes" 
+
+if num>=18:
+    if a == "Yes":
+        print("can drive")
+    else:
+        print ("Not drive")  
+          
+    
