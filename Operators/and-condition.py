4d8b64a3e1bb9af69f4c837b7e391d2a0e0faa76
Madhu <madhusingh14061996@gmail.com>
2025-05-25 02:22:45 +0530

    And Conditions

---
+++ Operators/and-condition.py
@@ -0,0 +1,10 @@
+# AND Condition
+# Write a program to check if a number is between 10 and 20 (inclusive) using and and comparison operators.
+
+num=13
+if num  >=10 and num<=20:
+    print("num is betwwwn 10 and 20")
+else:
+    print("num not match")    
+
+
