892acf10359125cc86894f47302a58dcd584400d
Madhu <madhusingh14061996@gmail.com>
2025-05-25 02:33:55 +0530

    or conditions

---
+++ Operators/or-condition.py
@@ -0,0 +1,10 @@
+# OR Condition
+# Write a program to check if a character is a vowel ('a', 'e', 'i', 'o', 'u') using multiple or conditions.
+
+char="a"
+
+if char == "a" or char=="e"or char=="i"or char=="o"or char=="u":
+    print("The char is vowel")
+else:
+    print("The char is not vowel")
+
