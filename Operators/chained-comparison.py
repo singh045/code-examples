43a2e79df166df9628c0b1f63c7000f42141013e
Madhu <madhusingh14061996@gmail.com>
2025-05-25 03:16:16 +0530

    nested conditions

---
+++ Operators/chained-comparison.py
@@ -0,0 +1,7 @@
+# Chained Comparison
+# Use a chained comparison to check if a variable x lies strictly between 0 and 1 (i.e., 0 < x < 1).
+
+
+
+
+
---
+++ Operators/nested-conditions.py
@@ -0,0 +1,17 @@
+# Nested Logical Conditions
+# Ask for two integers a and b.
+# If a > 0 and b > 0, print "Both positive".
+# Else if a < 0 or b < 0, print "At least one negative".
+
+a=int(input("Enter int a:"))
+b=int(input("Enter int b:"))
+
+if a>0 and b>0:
+    print ("Both positive")
+else:
+    print("At lease one Negative")    
+
+
+
+
+
