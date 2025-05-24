22953447ccdede23400dad8fdd35a0cd4a0f8f6f
Madhu <madhusingh14061996@gmail.com>
2025-05-24 20:20:39 +0530

    vowel checker

---
+++ vowel.py
@@ -0,0 +1,7 @@
+# Nested If with Strings
+# Write a program that checks if a given word starts with a vowel.
+# If true, check if it also ends with a vowel
+# Print "Starts and ends with vowel" or a suitable message otherwise
+
+vowel="orange"
+if 
