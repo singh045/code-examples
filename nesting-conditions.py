e07b7b487a3742601cc6c25d0c41631eed49370a
Madhu <madhusingh14061996@gmail.com>
2025-05-24 14:29:08 +0530

    nesting conditions

---
+++ nesting-conditions.py
@@ -0,0 +1,12 @@
+# Multiple Conditions with Nesting
+# Write a program that checks if a number is divisible by 2. If true, check if it's also divisible by 3.
+# Print "Divisible by both 2 and 3"
+# If only divisible by 2, print "Only divisible by 2"?
+num=6
+if num%2==0:
+    if num%3==0:
+        print("Divisible by both 2 and 3")
+    else:
+        print("Only divisible by 2")   
+
+    
