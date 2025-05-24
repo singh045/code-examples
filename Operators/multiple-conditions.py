e8c9ff8ae582a998426abdd02314379740f8ff63
Madhu <madhusingh14061996@gmail.com>
2025-05-25 02:52:22 +0530

    multiple conditions

---
+++ Operators/multiple-conditions.py
@@ -0,0 +1,14 @@
+# Multiple Conditions
+# Given a student's marks, check if the marks are greater than or equal to 50 and less than or equal to 100.
+# If true, print "Pass", else "Fail".
+
+marks=61
+if marks >=50:
+    print("Pass")
+elif marks <=100:
+    print("Pass")    
+else:
+    print("Fail")
+
+
+
