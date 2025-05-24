08155dabe7c5a6430938e2df6cb18d321d57be58
Madhu <madhusingh14061996@gmail.com>
2025-05-25 00:34:00 +0530

    variable checker a and b are equal or not

---
+++ equality-checker.py
@@ -0,0 +1,13 @@
+# Equality Check
+# Write a Python program to check if two variables a and b are equal using ==.
+a=10
+b=10
+if a==b:
+    print("a and b are equal")
+else:
+    print("a and b are not equal")
+
+
+
+
+
