ebb2211792b19610f4d2f4cb136a81ab2b1f8363
Madhu <madhusingh14061996@gmail.com>
2025-05-25 01:18:35 +0530

    Age Eligibility checker

---
+++ Operators/age-eligbility.py
@@ -0,0 +1,19 @@
+# Age Eligibility
+# Ask the user for their age.
+# If age is greater than or equal to 18, print "Adult".
+# If less than 18, print "Minor".
+
+num=int(input("Enter Age:"))
+if num >=18:
+    print("Adult")
+    if num<18:
+        print("Minor")
+else:
+    print("Age Eligibility not matched")
+
+
+
+
+
+
+
