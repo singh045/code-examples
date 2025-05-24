8f41f899326fff9dcf0d50ae1c3cf558fffe44e7
Madhu <madhusingh14061996@gmail.com>
2025-05-25 03:03:03 +0530

    AND/OR Conditions

---
+++ Operators/and-or-condition.py
@@ -0,0 +1,13 @@
+# Combined AND/OR
+# Write a program that prints "Accepted" if a number is either:
+# greater than 100 or
+# between 50 and 60 (inclusive)
+
+num=11
+if num>=100 or num >=50 and num<=60:
+    print("Accepted")
+else:
+    print("Not Accepted")    
+
+
+
--- Operators/multiple-conditions.py
+++ Operators/multiple-conditions.py
@@ -2,10 +2,10 @@
 # Given a student's marks, check if the marks are greater than or equal to 50 and less than or equal to 100.
 # If true, print "Pass", else "Fail".
 
-marks=61
+marks=20
 if marks >=50:
     print("Pass")
-elif marks <=100:
+elif marks>=50 and marks<=100:
     print("Pass")    
 else:
     print("Fail")
