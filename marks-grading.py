cef2f82cb3d99ca062371347af8acc76e4243834
Madhu <madhusingh14061996@gmail.com>
2025-05-24 14:13:35 +0530

    add marks grading code

---
+++ marks-grading.py
@@ -0,0 +1,15 @@
+# Marks Grading
+# Write a Python program that takes a student's marks and prints their grade using nested if:
+# 90 and above: A
+# 80–89: B
+# 70–79: C
+# Below 70: Fail
+marks=70
+if marks>=90:
+    print("Grade A")
+elif marks >=80 and marks <=89:
+    print ("Grade B")   
+elif marks >=70 and marks <=79:
+    print("Grade C") 
+else:
+    print("Fail")  
