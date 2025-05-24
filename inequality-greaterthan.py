ec60db6c9b11a6b37951776b5cf322aede829d7d
Madhu <madhusingh14061996@gmail.com>
2025-05-25 00:53:14 +0530

    inequality checker and greater than checker

---
+++ inequality-greaterthan.py
@@ -0,0 +1,10 @@
+# Inequality and Greater Than
+# Given x = 10 and y = 5, write a condition to check if x is not equal to y and x is greater than y.
+
+x=1
+y=5
+if x!=y and x>y:
+    print("x and y is not equal and x is greater than y")
+else:
+    print("condition not match")    
+   
