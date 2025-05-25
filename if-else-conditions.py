5c1a4a24573869a41b2721cb94fc1e50537be4e1
Madhu <madhusingh14061996@gmail.com>
2025-05-25 18:22:06 +0530

    if else conditions

---
+++ if-else-conditions.py
@@ -0,0 +1,19 @@
+# Positive, Negative, or Zero
+# Take an integer input and use if-elif-else to determine if the number is positive, negative, or zero.
+
+num=int(input("Enter a number:"))
+if num>=1:
+    print("num is positive")
+elif num<0:
+    print ("num is negative")    
+else:
+    print("num is zero")    
+   
+
+
+
+
+
+
+
+
